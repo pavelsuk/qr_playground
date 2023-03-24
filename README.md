ee7a8fa9af1a77ea5a8b58af63ac6fd2cc48ad3c
pavelsuk <pavelsuk@users.noreply.github.com>
2023-03-24 16:06:15 +0100

    Modified example with minimalistic version

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Playing with examples for QR creation
 
 - [qrcode library](https://pypi.org/project/qrcode/)
 - [Generating QR Code for the vCard | proxyroot.com](https://proxyroot.com/vr-card-to-qr-code/)
+- [Generate vCard using Python | gist gerwin3](https://gist.github.com/gerwin3/ef9ae04d2b156c405db45dc70b93f716)
+- [VCard 4.0 Specification RFC 6350](https://www.rfc-editor.org/rfc/rfc6350)
 
 ## Installation requirements
 
--- from_proxyroot/generateqr.py
+++ from_proxyroot/generateqr.py
@@ -17,6 +17,25 @@ REV:20080424T195243Z
 x-qq:21588891
 END:VCARD"""
 
+"""
+# from https://gist.github.com/gerwin3/ef9ae04d2b156c405db45dc70b93f716
+return [
+        'BEGIN:VCARD',
+        'VERSION:2.1',
+        f'N:{last_name};{first_name}',
+        f'FN:{first_name} {last_name}',
+        f'ORG:{company}',
+        f'TITLE:{title}',
+        f'EMAIL;PREF;INTERNET:{email}',
+        f'TEL;WORK;VOICE:{phone}',
+        f'ADR;WORK;PREF:;;{address_formatted}',
+        f'REV:1',
+        'END:VCARD'
+    ]
+"""
+
+
+
 details = {
     "last_name": "Khan",
     "first_name": "Afroze",
@@ -39,6 +58,27 @@ details = {
     "home_address_pin": "NW1 6XE",
     "home_address_country": "United Kingdom",
     "email": "afroze@proxyroot.com",
+    "position": "Master of QR Codes",
+    "note": "Author of the example"
+}
+
+template_mini = """BEGIN:VCARD
+VERSION:4.0
+N:{last_name};{first_name}
+FN:{first_name} {last_name}
+ORG:{company}
+EMAIL:{email}
+TITLE:{position}
+NOTE: {note}
+END:VCARD"""
+
+details_mini = {
+    "last_name": "Kan Ban",
+    "first_name": "Scrumatha",
+    "company": "Keep It Simple Stupid, Ltd",
+    "position": "Agile Coach",
+    "email": "scrumatha@kiss.in",
+    "note": "Agile Conference 2024"
 }
 
 def create_qr_code_image(qr_code_data, file_path):
@@ -54,5 +94,11 @@ def create_qr_code_image(qr_code_data, file_path):
     img.save(file_path)
 
 data = template.format(**details)
+# 'BEGIN:VCARD\nVERSION:4.0\nN:Khan;Afroze;;Mr..;\nORG:proxyroot\nTITLE:Engineering Leader\nPHOTO;MEDIATYPE#image/gif:https://proxyroot.com\nTEL;TYPE#work,voice;VALUE#uri:tel:+1-917-000-0000\nTEL;TYPE#home,voice;VALUE#uri:tel:+1-917-000-0001\nADR;TYPE#WORK;PREF#1;LABEL#"proxyroot":;;proxyroot inc;Azgard City;AZ;00000;Azgard\nADR;TYPE#HOME;LABEL#"Holmes":;;221B Baker Street;London;11111;NW1 6XE;United Kingdom\nEMAIL:afroze@proxyroot.com\nREV:20080424T195243Z\nx-qq:21588891\nEND:VCARD'
+
 create_qr_code_image(data, "my-vrcard-qr-code.png")
+
+data_mini = template_mini.format(**details_mini)
+create_qr_code_image(data_mini, "my-vrcard-qr-code-mini.png")
+
 
